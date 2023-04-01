9ec1ef7f8cf5b747fcafd8465c74045b08891b1d
lcd1232 <8745863+lcd1232@users.noreply.github.com>
2023-04-01 21:23:33 +0400

    Update logic to have the same file each time

--- generate.py
+++ generate.py
@@ -21,10 +21,14 @@ if __name__ == "__main__":
     json_data = {
         "app_id": [],
     }
+    files = []
     for file_name in os.listdir(directory):
         if file_name.endswith(".yml"):
-            file_path = os.path.join(directory, file_name)
-            json_data["app_id"].append(convert_yaml_to_json(file_path))
+            files.append(file_name)
+    files.sort()
+    for file_name in files:
+        file_path = os.path.join(directory, file_name)
+        json_data["app_id"].append(convert_yaml_to_json(file_path))
     with open(os.path.join("v1", "data.json"), "w") as f:
         json.dump(json_data, f, indent=2)
         f.write("\n")  # Add a newline character at the end of the file
