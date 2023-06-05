984ccdd8de1f6f62659a6d9d2af5d7168dc6eb29
lcd1232 <8745863+lcd1232@users.noreply.github.com>
2023-06-06 02:31:12 +0400

    Improve code

--- create-appids.py
+++ create-appids.py
@@ -1,14 +1,10 @@
 import argparse
-import datetime
-import json
 import os
-import re
 import time
 import typing
-from urllib.parse import quote
 
-import requests
 import yaml
+from pytube import Search
 from requests_ratelimiter import LimiterSession
 
 session = LimiterSession(per_minute=40)
@@ -33,53 +29,23 @@ def write_to_file(gameid: str, game_name: str, youtube_link: str) -> None:
         )
 
 
-def text_to_timedelta(text: str) -> datetime.timedelta:
-    splits = text.split(":")
-    if len(splits) == 2:
-        minutes, seconds = splits
-        return datetime.timedelta(minutes=int(minutes), seconds=int(seconds))
-    elif len(splits) == 3:
-        hours, minutes, seconds = splits
-        return datetime.timedelta(
-            hours=int(hours), minutes=int(minutes), seconds=int(seconds)
-        )
-    else:
-        raise ValueError(f"Invalid time format: {text}")
-
-
 def get_youtube_link(game_name) -> typing.Optional[str]:
     search_query = f"{game_name} main theme"
-    search_query_encoded = quote(search_query)
+    s = Search(search_query)
 
-    # Search on YouTube
-    youtube_url = f"https://www.youtube.com/results?search_query={search_query_encoded}&sp=QgIIAQ%3D%3D"
-
-    # Fetch YouTube search results
-    response = requests.get(youtube_url)
-    result = re.findall(r"ytInitialData\s*=\s*({.*?});", response.text)
-    if not result:
+    if len(s.results) == 0:
+        print(f"Failed to find any results for {game_name}")
         return None
-    data = result[0]
-    data = json.loads(data)
-    for content in data["contents"]["twoColumnSearchResultsRenderer"][
-        "primaryContents"
-    ]["sectionListRenderer"]["contents"]:
-        if "itemSectionRenderer" in content:
-            for item in content["itemSectionRenderer"]["contents"]:
-                if "videoRenderer" in item:
-                    video = item["videoRenderer"]
-                    if "lengthText" not in video:
-                        print(f"Skipping video since it's live - {video['videoId']}")
-                        continue
-                    duration = text_to_timedelta(video["lengthText"]["simpleText"])
-                    video_id = video["videoId"]
-                    if duration.seconds == 0:
-                        print(f"Skipping video with empty length - {video_id}")
-                        continue
-                    if duration > datetime.timedelta(minutes=5):
-                        print(f"Skipping video with length > 5 minutes - {video_id}")
-                        continue
-                    return f"https://www.youtube.com/watch?v={video_id}"
+
+    for result in s.results:
+        if not result.length:
+            continue
+        if result.length > 300:
+            print(
+                f"Skipping too long video: {result.video_id} ({result.length}) - {result.title}"
+            )
+            continue
+        return result.watch_url
     return None
 
 
@@ -102,6 +68,8 @@ def main():
     )
     data: dict = response.json()
     print(f"Found {len(data['applist']['apps'])} apps")
+    # Sort the apps by appid descending
+    data["applist"]["apps"].sort(key=lambda x: x["appid"])
     for i, app in enumerate(data["applist"]["apps"]):
         if args.start is not None:
             if not passed:
@@ -113,6 +81,9 @@ def main():
         print(f"Checking {app['appid']} - {app['name']}")
         app_id = app["appid"]
         app_name = app["name"]
+        if os.path.exists(os.path.join("games", f"{app_id}.yml")):
+            print(f"Skipping {app_id} - {app_name} since it already exists")
+            continue
         app_information = get_app_information(app_id)
         if not app_information["success"]:
             print(f"Failed to get information for {app_id} - {app_name}")
@@ -120,9 +91,6 @@ def main():
         if not app_information["data"]["type"] == "game":
             continue
         print(f"Found {app_id} - {app_name}")
-        if os.path.exists(os.path.join("games", f"{app_id}.yml")):
-            print(f"Skipping {app_id} - {app_name} since it already exists")
-            continue
         youtube_link = get_youtube_link(app_name)
         if youtube_link:
             write_to_file(app_id, app_name, youtube_link)
@@ -131,3 +99,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+    # s = Search("zelda live")
+    # print(len(s.results))
+    # for result in s.results:
+    #     print(result, result.title, result.length, result.metadata)
